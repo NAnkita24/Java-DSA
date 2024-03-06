383658f33f25b4f13d4c8005f531115e59ba00e6
NAnkita24 <apjagtap24@gmail.com>
2024-03-06 18:20:34 +0530

    6 Mrach

--- src/array/PrefixSum.java
+++ src/array/PrefixSum.java
@@ -1,9 +1,9 @@
 package array;
 
 public class PrefixSum {
-public static void main(String[] args) {
-	int arr[]= {4,6,2,11,20,25,30};
-	
-	
-}
+	public static void main(String[] args) {
+		int arr[]= {4,6,2,11,20,25,30};
+
+
+	}
 }
--- src/array/RemoveDuplicate.java
+++ src/array/RemoveDuplicate.java
@@ -4,7 +4,7 @@ public class RemoveDuplicate {
 	public static void main(String[] args) {
 		int arr[]= {2,4,4,7,9,11,15,15,21,39,57,57,88};
 		int count=0;
-		
+
 		for (int i = 0; i < arr.length; i++) {
 			for (int j = i+1; j < arr.length; j++) {
 				if (arr[i]==arr[j]) {
@@ -12,23 +12,23 @@ public class RemoveDuplicate {
 				}
 			}
 		}
-		
+
 		System.out.println(count);
-		
-		
+
+
 		int arr1[]=new int[arr.length-count];
 		int j=0;
 		for (int i = 0; i < arr.length-1; i++) {
-				if (arr[i]!=arr[i+1]) {
-					arr1[j] = arr[i];
-					j++;
-				}
+			if (arr[i]!=arr[i+1]) {
+				arr1[j] = arr[i];
+				j++;
+			}
 		}
 		arr1[j] = arr[arr.length-1];
 		for (int i = 0; i < arr1.length; i++) {
 			System.out.print(arr1[i]+" ");
 		}
-		
-		
+
+
 	}
 }
